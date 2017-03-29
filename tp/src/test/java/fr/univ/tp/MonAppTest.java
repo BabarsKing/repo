1b1a0f89860ef908a4e16f149ddb9e68f716981c
terrillonjc <terrillonjc@MI103-03>
2017-03-29 18:08:42 +0200

    Share project

---
+++ tp/src/test/java/fr/univ/tp/MonAppTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.univ.tp;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author terrillonjc
+ *
+ */
+public class MonAppTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
